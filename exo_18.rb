aa02c1d71d019d21660f1a87b8f6d33533f141c1
evapapazian <eva.papazian@ieseg.fr>
2019-10-01 17:17:16 +0200

    changement nom exo 17 + exo 18

---
+++ exo_18.rb
@@ -0,0 +1,26 @@
+
+tableau_email = []  #il sait que le tableau email existe, même s'il ne contient pas encore de valeur
+
+
+
+
+	i = 0
+	while i < 50
+		i = i + 1
+
+
+if i <10
+	puts "jean.dupont.0#{i}@email.fr" 
+	tableau_email [i] ="jean.dupont.0#{i}@email.fr"
+
+else 
+	puts "jean.dupont.#{i}@email.fr"
+	tableau_email [i] ="jean.dupont.#{i}@email.fr"
+end
+end
+
+
+puts tableau_email 
+
+# la première liste qui s'affiche n'appartient pas au tableau
+# la liste qui s'affiche ensuite correspond au puts tableau_email aka afficher ce qu'il y a dans le tableau
