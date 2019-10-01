6377b4892f5f1b48a7aa832c33dfb4473aeb6aac
Pol-Erwan <polerwankite@gmail.com>
2019-10-01 12:12:04 +0200

    exo7_a_b_c

---
+++ exo_07_a.rb
@@ -0,0 +1,3 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp #gets.chomp ouvre un dialogue avec l'utilisateur
+puts user_name #affiche la réponse entrée par l'utilisateur grâce à la commande "get.chomp"
---
+++ exo_07_c.rb
@@ -0,0 +1,15 @@
+user_name = gets.chomp
+puts user_name
+
+#7a
+#puts "Bonjour, c'est quoi ton blase ?"
+#user_name = gets.chomp -----> gets.chomp ouvre un dialogue avec l'utilisateur
+#puts user_name  -----> affiche la réponse entrée par l'utilisateur grâce à la commande "get.chomp" en appuyant sur entrée
+
+
+#puts "Bonjour, c'est quoi ton blase ?"
+#print "> "  -----> affiche le symbole ">" sans revenir à la ligne
+#user_name = gets.chomp -----> ouvre un dialogue avec l'utilisateur
+#puts user_name ----->  affiche la réponse entrée par l'utilisateur grâce à la commande "get.chomp" en appuyant sur entrée
+
+#exo7c juste entrer des caractères et les programme les affichera une fois appuyer sur entrée
