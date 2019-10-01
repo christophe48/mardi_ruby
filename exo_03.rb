4b001b623494ccf8a77c61192ca9831878f0395f
Pol-Erwan <polerwankite@gmail.com>
2019-10-01 11:30:00 +0200

    exo3

---
+++ exo_03.rb
@@ -0,0 +1,4 @@
+puts "Bonjour monde !"
+
+#puts "Et avec une voix sexy, ça donne: Bonjour, monde!"
+#le symbole # permet de mettre une ligne en commentaire parmis les ligne de code, c'est pour celà qu'on ne le voit pas.
