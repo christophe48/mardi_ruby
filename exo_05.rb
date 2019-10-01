56c95c98bed8177f09408173d8cc74c929f3b271
Pol-Erwan <polerwankite@gmail.com>
2019-10-01 11:47:19 +0200

    exo5

---
+++ exo_05.rb
@@ -0,0 +1,26 @@
+puts "On va compter le nombre d'heures de travail à THP" #affichage du texte
+puts "Travail : #{10 * 5 * 11}" #affiche du texte entre " et de du résultat de l'opération entre {}
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" #affiche du texte entre " et de du résultat de l'opération entre {}
+
+
+puts "Et en secondes ?" #affichage du texte entre"
+
+puts 10 * 5 * 11 * 60 * 60 #affichage du résultat
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" #affichage du texte entre "
+
+puts 3 + 2 < 5 - 7 #affichage du résultat booléen avec le symbole <
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" #affichage texte entre " et affichage du résultat dans le string {}
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" #affichage texte entre " et affichage du résultat dans le string {}
+
+
+puts "Ok, c'est faux alors !" #affichage texte entre "
+
+puts "C'est drôle ça, faisons-en plus :" # afichage texte entre "
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" # affichage texte entre " et résultat booléen dans le string {}
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" # affichage texte entre " et résultat booléen dans le string {}
+
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" # affichage texte entre " et résultat booléen dans le string {}
+
