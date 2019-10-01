38d2232ebc0b894a1102d766eee33f81a59ed5d4
Pol-Erwan <polerwankite@gmail.com>
2019-10-01 18:41:24 +0200

    all exercise

--- exo_21.rb
+++ exo_21.rb
@@ -1,11 +1,17 @@
 puts "Dis un nombre entre 0 et 25"
 print "> "
 p = gets.chomp.to_i
+puts "voici la pyramide :"
+
 n=1
+
 while n <= p
+  m= p-n
     if (n == 1)
+        print (" " * m)
         puts ("#" * n)
     else
+        print (" " * m)
         puts ("#" * n)
     end
     n += 1
